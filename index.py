6e3d9354ae1a8ef84eb828bafb442f2b373f39e8
vologue <raghav.sp98@gmail.com>
2019-04-20 18:20:20 +0530

    Cleared bugs

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from werkzeug import secure_filename
+import os
+
+
 app = Flask(__name__)
-UPLOAD_FOLDER="/tmp"
+UPLOAD_FOLDER="tmp"								#/at the begining will save it in /tmp (root of your Linux machine)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def index_page():
 	return render_template('index.html')
@@ -10,14 +15,13 @@ def index_page():
 def file_upload():
 	return render_template('upload.html')
 
-
 @app.route('/uploader', methods=['POST'])
-def file():
+def files():											#cant have keyword as function
 	if request.method == 'POST':
-		file = request.files['file']
-      	filename = secure_filename(file.filename)
-      	f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		fil = request.files['myfile']						#cant have keyword as variable
+      	fil.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(fil.filename)))
       	return render_template('uploaded.html')
+
 
 if __name__=="__main__":
 	app.run(debug=True)
